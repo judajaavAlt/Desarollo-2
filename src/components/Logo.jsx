98936788da8f8b4660bc947b5bb080805ba6dba3
Brayan Steven Narvez Valdes <brayan.steven.narvaez@correounivalle.edu.co>
2024-10-17 15:26:54 -0500

    Snapshot 17 10

---
+++ src/components/Logo.jsx
@@ -0,0 +1,15 @@
+export default function Logo() {
+  return (
+    <div className="px-8 py-2">
+      <p className="flex items-end text-3xl font-bold">
+        {" "}
+        <img
+          src="../public/login-assets/logo-login.png"
+          alt="logo app"
+          className="h-16 scale-x-[-1] transform"
+        />
+        CATMONEY
+      </p>
+    </div>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+import Login from './pages/Login';
+import SingUp from './pages/SingUp';
+import Home from './pages/Home';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <h1>pagina</h1>
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to={"/login"} />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/singup" element={<SingUp />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
   </StrictMode>,
 )
