866f15a069de321427a3c5c06750a8cb3be15985
JuanCarlos-developer <juan.cruz.munoz@correounivalle.edu.co>
2024-10-20 14:39:59 -0500

    added testReadWallet

--- __tests__/testUnit.test.js
+++ __tests__/testUnit.test.js
@@ -1,5 +1,10 @@
 import { createWallet, deleteWallet, readWallet, updateWallet } from "../src/helpers/portWallets";
 
-const testCreateWallet = ()=>{return expect(createWallet).not.toThrow(Error)};
+const testCreateWallet = ()=>{return expect(createWallet).not.toThrow(Error);};
 
 test("Create wallet test", testCreateWallet);
+
+
+const testReadWallet = ()=>{return expect(readWallet).not.toThrow(Error);};
+
+test("Read wallet test", testReadWallet);
