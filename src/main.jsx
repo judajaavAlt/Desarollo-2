596cc2c8b6cb5d1d23b706d892bd4101b769ee79
Brayan Steven Narvaez Valdes <brayanss2018@gmail.com>
2024-11-20 16:18:11 -0500

    inplementation login

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+import Login from './pages/Login';
+import SingUp from './pages/SingUp';
+import Home from './pages/Home';
 import CategoryList from './components/Category/CategoryList';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <CategoryList></CategoryList>
-  </StrictMode>
-);
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to={"/login"} />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/singup" element={<SingUp />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/category" element={<CategoryList />} />
+        </Routes>
+      </BrowserRouter>
+  </StrictMode>,
+)
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import Logo from "../components/Logo";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-purple-400">
+      <Logo></Logo>
+    </div>
+  );
+}
