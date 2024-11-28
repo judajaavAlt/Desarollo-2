d25d4f1bf28a2236fd61fc705a9ef6b52913d795
Brayan Steven Narvaez Valdes <brayanss2018@gmail.com>
2024-11-28 00:42:59 -0500

    fix errors creat and read

--- src/components/Transactions/CreateTransaction/CreateTransaction.jsx
+++ src/components/Transactions/CreateTransaction/CreateTransaction.jsx
@@ -11,25 +11,24 @@ const CreateTransaction = ({ isOpen, onClose }) => {
   const [cat, setCat] = useState({
     from: "",
     destination: "",
-    transactionAmount: "",
+    transactionAmount: 0,
     transactionName: "",
     transactionDate: "",
   });
 
-  // useEffect(() => {
-  //   if (isCreate) {
-  //     setCat({
-  //       from: "",
-  //       destination: "",
-  //       transactionAmount: "",
-  //       transactionName: "",
-  //       transactionDate: "",
-  //     });
-  //   } else {
-  //     setCat(infoTransaction);
-  //   }
-  //   setErrors({ name: false});
-  // }, [isOpen]);
+  const [dataWallet, setDataWallet] = useState([]);
+
+  useEffect(() => {
+    const traer = async () => {
+      try {
+        const obtener = await readWallet(1);
+        setDataWallet(obtener);
+      } catch (e) {
+        console.error("Error al cargar categorías:", e);
+      }
+    };
+    traer();
+  }, []);
 
   const handleNameChange = (event) => {
     const newValue = event.target.value;
@@ -38,53 +37,61 @@ const CreateTransaction = ({ isOpen, onClose }) => {
       ...prevState,
       [nameProp]: newValue,
     }));
-    console.log(event.target.name);
     console.log(cat);
   };
 
   const llamoo = async () => {
-      try {
-        const transactionDate = "2024-08-27"; // Fecha de ejemplo
-        const transactionName = "Compra en tienda X";
-        const transactionAmount = 150; // Monto de ejemplo
-        const destination = 1;
-        const from = 2;
-    
-        await createTransaction(transactionDate, transactionName, transactionAmount, destination, from);
-        console.log("Transacción creada exitosamente.");
-      } catch (error) {
-        console.error("Hubo un problema al crear la transacción:", error.message);
-      }
-    ;
-    // const updatedCat = {
-    //   ...cat,
-    //   transactionDate: new Date(cat.transactionDate).toISOString(), // Cambia al formato ISO
-    // };
-  
-    // createTransaction(
-    //   updatedCat
-    // );
-  
-    // console.log("Datos enviados a la BD:", updatedCat);
-    // createTransaction(cat)
-    // console.log(cat)
-  }
+    try {
+      await createTransaction(
+        cat.transactionDate,
+        cat.transactionName,
+        cat.transactionAmount,
+        cat.destination,
+        cat.from
+      );
+      console.log("Transacción creada exitosamente.");
+      setCat({
+        from: "",
+        destination: "",
+        transactionAmount: 0,
+        transactionName: "",
+        transactionDate: "",
+      });
+      onClose();
+    } catch (error) {
+      console.error("Hubo un problema al crear la transacción:", error.message);
+    }
+  };
 
   return (
     <Modal isOpen={isOpen} onClose={onClose} title="Crear transferencia">
       <InfoGroup label="Transfiera de:">
         <select name="from" onChange={handleNameChange} value={cat.from}>
-          <option value={1}>Efectivo</option>
-          <option value={2}>Bancolombia</option>
-          <option value={3}>Nequi</option>
+          <option value="">Seleccione</option>
+          {dataWallet
+            .filter((w) => w.walletID + "" !== cat.destination) // Filtrar la opción seleccionada en "destination"
+            .map((w) => (
+              <option key={w.walletID} value={w.walletID}>
+                {w.walletName}
+              </option>
+            ))}
         </select>
       </InfoGroup>
 
       <InfoGroup label="Transfiera a:">
-        <select name="destination" onChange={handleNameChange} value={cat.destination}>
-          <option value={1}>Efectivo</option>
-          <option value={2}>Bancolombia</option>
-          <option value={3}>Nequi</option>
+        <select
+          name="destination"
+          onChange={handleNameChange}
+          value={cat.destination}
+        >
+          <option value="">Seleccione</option>
+          {dataWallet
+            .filter((w) => w.walletID + "" !== cat.from) // Filtrar la opción seleccionada en "from"
+            .map((w) => (
+              <option key={w.walletID} value={w.walletID}>
+                {w.walletName}
+              </option>
+            ))}
         </select>
       </InfoGroup>
 
@@ -92,22 +99,30 @@ const CreateTransaction = ({ isOpen, onClose }) => {
         <input
           name="transactionAmount"
           type="number"
-          defaultValue={0}
           onChange={handleNameChange}
           value={cat.transactionAmount}
         />
       </InfoGroup>
 
       <InfoGroup label="Fecha:">
-        <input type="date" name="transactionDate" onChange={handleNameChange} value={cat.transactionDate}/>
+        <input
+          type="date"
+          name="transactionDate"
+          onChange={handleNameChange}
+          value={cat.transactionDate}
+        />
       </InfoGroup>
 
       <InfoGroup label="Comentarios:">
-        <textarea name="transactionName" onChange={handleNameChange} value={cat.transactionName}></textarea>
+        <textarea
+          name="transactionName"
+          onChange={handleNameChange}
+          value={cat.transactionName}
+        ></textarea>
       </InfoGroup>
 
       <div className="button-group">
-        <Button text="Crear" className="button-create" onClick={llamoo}/>
+        <Button text="Crear" className="button-create" onClick={llamoo} />
       </div>
     </Modal>
   );
--- src/components/Transactions/TransactionList.jsx
+++ src/components/Transactions/TransactionList.jsx
@@ -4,7 +4,6 @@ import { readTransaction } from "../../helpers/portTransaccion";
 import { readWallet } from "../../helpers/portWallets";
 import ReadTransaction from "./ReadTransaction/ReadTransaction";
 
-
 // const transactions = [
 //   { from: 'Principal', to: 'Efectivo', amount: '1.000.000', date: '22 de septiembre de 2024' },
 //   { from: 'Nequi', to: 'Principal', amount: '1.000.000', date: '22 de septiembre de 2024' },
@@ -13,47 +12,35 @@ import ReadTransaction from "./ReadTransaction/ReadTransaction";
 //   { from: 'Daviplata', to: 'Tarjeta débito', amount: '220.000', date: '22 de septiembre de 2024' },
 // ];
 
-
-
 function TransactionList() {
   const [infoTransaction, setInfoTransaction] = useState(null);
-  const [isModalOpenCreate, setIsModalOpenCreate ] = useState(false);
-  const [isModalOpenRead, setIsModalOpenRead ] = useState(false);
-
+  const [isModalOpenCreate, setIsModalOpenCreate] = useState(false);
+  const [isModalOpenRead, setIsModalOpenRead] = useState(false);
   const [dataWallet, setDataWallet] = useState([]);
-
-  useEffect(() => {
-    const traer = async () => {
-      try {
-        const obtener = await readWallet(1);
-        setDataWallet(obtener);
-        //console.log(obtener);
-      } catch (e) {
-        console.error("Error al cargar categorías:", e);
-      }
-    };
-    traer();
-  }, []);
-
-
   const [dataTranstation, setDataTranstation] = useState([]);
 
   useEffect(() => {
     const traer = async () => {
       try {
-        const obtener = await readTransaction(1, 8, 2024);
+        const giveWallet = await readWallet(1);
+        setDataWallet(giveWallet);
+
+        const now = new Date();
+        const mesActual = now.getMonth() + 1; // getMonth() devuelve un índice de 0 (enero) a 11 (diciembre), por eso se suma 1.
+        const anioActual = now.getFullYear();
+
+        const obtener = await readTransaction(1, mesActual, anioActual);
         setDataTranstation(obtener);
-         console.log(obtener);
       } catch (e) {
         console.error("Error al cargar categorías:", e);
       }
     };
     traer();
-  }, []);
+  }, [isModalOpenCreate, isModalOpenCreate]);
 
   const openModalRead = (data) => {
     setIsModalOpenRead(true);
-    setInfoTransaction(data)
+    setInfoTransaction(data);
   };
 
   const closeModalRead = () => {
@@ -67,24 +54,50 @@ function TransactionList() {
   const closeModalCreate = () => {
     setIsModalOpenCreate(false);
   };
+
   return (
     <div className="transaction-list">
-      <button className="create-transaction-btn" onClick={openModalCreate}>Crear Transferencia</button>
+      <button className="create-transaction-btn" onClick={openModalCreate}>
+        Crear Transferencia
+      </button>
       <div className="transactions">
         {dataTranstation.map((transaction, index) => (
-          <div key={index} onClick={()=> openModalRead(transaction)} className="transaction-item">
+          <div
+            key={index}
+            onClick={() => openModalRead(transaction)}
+            className="transaction-item"
+          >
             <p>{transaction.transactionDate}</p>
             <div className="transaction-details">
-              <span>{dataWallet[transaction.from - 1].walletName}</span>
+              <span>
+                {
+                  dataWallet.find(
+                    (wallet) => wallet.walletID === transaction.from
+                  ).walletName
+                }
+              </span>
               <span className="arrow">→</span>
-              <span>{dataWallet[transaction.destination - 1].walletName}</span>
+              <span>
+                {
+                  dataWallet.find(
+                    (wallet) => wallet.walletID === transaction.destination
+                  ).walletName
+                }
+              </span>
               <span>{transaction.transactionAmount} COL$</span>
             </div>
           </div>
         ))}
       </div>
-      <CreateTransaction isOpen={isModalOpenCreate} onClose={closeModalCreate} />
-      <ReadTransaction isOpen={isModalOpenRead} onClose={closeModalRead} infoTransaction={infoTransaction} />
+      <CreateTransaction
+        isOpen={isModalOpenCreate}
+        onClose={closeModalCreate}
+      />
+      <ReadTransaction
+        isOpen={isModalOpenRead}
+        onClose={closeModalRead}
+        infoTransaction={infoTransaction}
+      />
     </div>
   );
 }
