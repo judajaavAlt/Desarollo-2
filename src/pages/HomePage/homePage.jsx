0aa200e78e25420647631fee6e48d5a95255ee72
judajaavAlt <132619919+judajaavAlt@users.noreply.github.com>
2024-11-29 23:35:45 -0500

    fix

--- src/pages/HomePage/homePage.jsx
+++ src/pages/HomePage/homePage.jsx
@@ -1,19 +1,11 @@
-import { React } from 'react';
-import HomeGraphic from '../../components/Home/HomeGraphic';
-import HomeMovementsList from '../../components/Home/HomeMovementsList';
-import './homePage.css'; 
+import HomeGraphic from "../../components/Home/HomeGraphic";
+import "./homePage.css";
 
 const HomePage = () => {
   return (
     <div className="home-page">
-      {/* Sección de los gráficos (parte superior) */}
       <section className="home-graphic-section">
         <HomeGraphic />
-      </section>
-
-      {/* Sección de la lista de movimientos (parte inferior) */}
-      <section className="home-movements-section">
-        <HomeMovementsList />
       </section>
     </div>
   );
