4e3bb7763cb78f24720a2714e509b89392018066
Brayan Steven Narváez Valdez <149515002+stevenD18skz@users.noreply.github.com>
2024-12-01 23:44:08 -0500

    modifique home

--- src/components/Home/HomeGraphic.jsx
+++ src/components/Home/HomeGraphic.jsx
@@ -7,10 +7,6 @@ const HomeGraphic = () => {
       <div className="total-amount">
         <h3>Total: $1,000,000</h3>
       </div>
-      <div className="graphics-container">
-        <img src="../../assets/images/bar-graph-placeholder.png" alt="Gráfico de barras" />
-        {/* Aquí puedes agregar una librería de gráficos para dibujar el gráfico */}
-      </div>
     </div>
   );
 };
--- src/components/Layout/layout.jsx
+++ src/components/Layout/layout.jsx
@@ -1,12 +1,12 @@
 // src/Layout.jsx
-import Sidebar from "../Sidebar/sidebar";
+//import Sidebar from "../Sidebar/sidebar";
 import { Outlet } from "react-router-dom";
 import "./layout.css";
 
 export default function Layout() {
   return (
     <div className="app-layout">
-      <Sidebar />
+      {/*<Sidebar />*/}
       <div className="content">
         <Outlet />
       </div>
